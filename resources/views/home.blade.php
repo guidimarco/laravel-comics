b758eee56bdbd4208d8cc01b6858c05006bdbbbd
Marco Guidi <mark.88g@gmail.com>
2021-01-08 00:07:22 +0100

    + config comics + sec jumbotron + sec comics

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,29 @@
 
 @section('main-content')
     <main>
-        
+        @include('partials.jumbotron')
+
+        <section id="series">
+            <div class="container">
+                <!-- title -->
+                <h1>Current series</h1>
+
+                <!-- container of comics -->
+                <div class="comics-container">
+                    @foreach ($comics as $current_comics)
+                        <!-- single comics card -->
+                        <div class="comics-card">
+                            <img src="{{ $current_comics['thumb'] }}" alt="comics {{ $current_comics['title'] }}">
+                            <h3>{{ $current_comics['series'] }}</h3>
+                        </div>
+                    @endforeach
+                </div>
+
+                <!-- btn -> load more -->
+                <div class="btn-container">
+                    <a href="#" class="btn btn-primary">Load more</a>
+                </div>
+            </div>
+        </section>
     </main>
 @endsection
