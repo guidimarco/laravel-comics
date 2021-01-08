7f6833a11c2058126cfda9dbcefc5cfa9338e9a2
Marco Guidi <mark.88g@gmail.com>
2021-01-08 14:09:19 +0100

    + comics cover (top) stile

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -4,6 +4,27 @@
     <main>
         @include('partials.jumbotron')
 
-        <h1>{{ $current_comics['title'] }}</h1>
+        <section id="comics">
+            <!-- top: comics cover -->
+            <div class="cover">
+                <div class="small-container">
+                    <div class="img-container">
+                        <!-- abs-span - mantain position -->
+                        <span>Comic book</span>
+                        <span>View gallery</span>
+
+                        <!-- img -->
+                        <img src="{{ $current_comics['thumb'] }}" alt="{{ $current_comics['title'] }}">
+                    </div>
+                </div>
+            </div>
+
+            <div class="info">
+                <div class="small-container">
+                    <h1>{{ $current_comics['title'] }}</h1>
+
+                </div>
+            </div>
+        </section>
     </main>
 @endsection
